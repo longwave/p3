c3c6cb259571f0253150a317769967e082c66481
Phil Clothier (cheldon) <philcloth@gmail.com>
2015-08-16 01:02:40 +0100

    clip the wings of Jetpack and Mojo

--- p3.php
+++ p3.php
@@ -83,8 +83,87 @@ function pipdig_p3_emmmm_heeey() {
 }
 add_action('wp_footer','pipdig_p3_emmmm_heeey');
 
+// get rid of some bad jetpack smells
+function pipdig_p3_kill_jetpack_modules( $modules, $min_version, $max_version ) {
+        $jp_mods_to_disable = array(
+        // 'shortcodes',
+        // 'widget-visibility',
+        // 'contact-form',
+        // 'shortlinks',
+         'infinite-scroll',
+        // 'wpcc',
+         'tiled-gallery',
+         'json-api',
+        // 'publicize',
+        // 'vaultpress',
+        // 'custom-css',
+         'post-by-email',
+        // 'widgets',
+        // 'comments',
+         'minileven',
+        // 'latex',
+         'gravatar-hovercards',
+        // 'enhanced-distribution',
+        // 'notes',
+        // 'subscriptions',
+        // 'stats',
+        // 'after-the-deadline',
+        // 'carousel',
+         'photon',
+         'sharedaddy',
+         'omnisearch',
+        // 'mobile-push',
+        // 'likes',
+        // 'videopress',
+        // 'gplus-authorship',
+        // 'sso',
+         'monitor',
+         'markdown',
+        // 'verification-tools',
+         'related-posts',
+        // 'custom-content-types',
+         'site-icon',
+        // 'protect',
+    );
+    foreach ( $jp_mods_to_disable as $mod ) {
+        if ( isset( $modules[$mod] ) ) {
+            unset( $modules[$mod] );
+        }
+    }
+    return $modules;
+}
+add_filter( 'jetpack_get_available_modules', 'pipdig_p3_kill_jetpack_modules', 20, 3 );
+
+// remove cruddy jetpack widgets
+function pipdig_p3_jetpack() {
+	remove_action('widgets_init', 'jetpack_facebook_likebox_init');
+	remove_action('widgets_init', 'wpcom_social_media_icons_widget_load_widget');
+	remove_action('widgets_init', 'jetpack_top_posts_widget_init');
+	remove_action('widgets_init', 'jetpack_display_posts_widget');
+}
+add_action('jetpack_modules_loaded','pipdig_p3_jetpack');
 
 
+function pipdig_p3_plugins_loaded() {
+	remove_action( 'admin_menu', 'mm_main_menu' ); // remove mojo menu
+	remove_action('widgets_init', 'mm_register_widget'); // remove mojo widget
+	remove_action( 'admin_head-themes.php', 'mm_add_theme_button' ); // remove mojo theme menu item
+	remove_action( 'admin_menu', 'mm_add_theme_page' ); // remove mojo themes link
+	add_action( 'admin_head-themes.php', 'pipdig_p3_themes_top_link' ); // replace themes link...
+}
+add_action('plugins_loaded','pipdig_p3_plugins_loaded');
+
+function pipdig_p3_themes_top_link() {
+	if(!isset($_GET['page'])) {
+	?>
+	<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$('.add-new-h2').before('<a class="add-new-h2" href="http://www.pipdig.co/products/wordpress-themes?utm_source=wpmojo&utm_medium=wpmojo&utm_campaign=wpmojo" target="_blank">pipdig Themes</a>');
+	});
+	</script>
+	<?php
+	}
+}
 
 // updates
 require 'plugin-update-checker/plugin-update-checker.php';
