88fa088de106b3d4b8bda65ed44db7599d51d940
Phil Clothier (Office) <philcloth@gmail.com>
2015-08-28 21:11:04 +0100

    tweaks

--- inc/admin-menus/updates.php
+++ inc/admin-menus/updates.php
@@ -20,7 +20,7 @@ if (!function_exists('pipdig_updates_options_page')) {
 
 					<div class="postbox">
 
-						<h3>Update 1.4 - Shortcodes</h3>
+						<h3>Update 1.4 - Shortcodes and Snapcodes:</h3>
 
 						<div class="inside">
 							<p>We'll be creating a new selection of shortcodes over the next few weeks.  To start things off, you can now use the shortcode [pipdig_stars] to automatically add a start rating. Perfect for anyone who writes product reviews. <a href="<?php echo admin_url('admin.php?page=pipdig-shortcodes'); ?>">Click here to read more</a>.</p>
