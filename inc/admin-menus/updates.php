9833ca2f06defb96379750f43149297d73190fa0
Phil Clothier (Office) <philcloth@gmail.com>
2015-09-05 18:28:01 +0100

    bit more french

--- inc/admin-menus/updates.php
+++ inc/admin-menus/updates.php
@@ -23,7 +23,7 @@ if (!function_exists('pipdig_updates_options_page')) {
 						<h3>Update 1.6 - Improved YouTube Widget</h3>
 
 						<div class="inside">
-							<p>We have significantly improved the way our custom YouTube widget works.  Please note, you will need to <a href="<?php echo admin_url('widgets.php'); ?>">re-configure this widget</a> if you have already set it up.</p>
+							<p>We have greatly improved the way our custom YouTube widget works.  Please note, you will need to <a href="<?php echo admin_url('widgets.php'); ?>">re-configure this widget</a> if you have already set it up.</p>
 							<p>Other minor improvements to speed up your site's loading time.</p>
 						</div>
 						<!-- .inside -->
