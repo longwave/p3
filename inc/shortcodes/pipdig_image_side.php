7f04d7344a556f755d9c068398c79476c89a7e6d
Phil XPS15 <philcloth@gmail.com>
2016-01-06 12:58:02 +0000

    left/right image

--- inc/shortcodes/pipdig_image_side.php
+++ inc/shortcodes/pipdig_image_side.php
@@ -4,26 +4,28 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-// [pipdig_image_side title="poop" image="" position="left"]
+// [pipdig_image_side title="" image="" image_position="left"]
 function pipdig_p3_image_side( $atts, $content = null ) {
 	extract( shortcode_atts( array(
 		//'title' => 'In this photo',
 		//'titlecolor' => '',
 		//'titletextsize' => '',
 		'image' => '',
-		'position' => 'left'
+		'image_position' => 'left'
 	), $atts ) );
 	
 	$img_id = rand(1, 999999999);
 	$text_id = rand(1, 999999999);
-	$position_out = '';
-	switch ($position) {
-		case 'left':
-			$position_out = 'top:20px;left:21px;text-align:left';
-			break;
-		case 'right':
-			$position_out = 'top:20px;right:21px;text-align:right';
-			break;
+	
+	$img_output = '<img id="'.esc_attr($img_id).'" src="'.esc_url($image).'" alt="'.esc_attr(get_the_title()).'" />';
+	$text_output = '<div id="'.esc_attr($text_id).'" class="pipdig_image_side_text">'.do_shortcode($content).'</div>';
+	
+	if ($image_position == 'left') {
+		$left = $img_output;
+		$right = $text_output;
+	} else {
+		$left = $text_output;
+		$right = $img_output;
 	}
 	
 	$output = '';
@@ -42,15 +44,11 @@ function pipdig_p3_image_side( $atts, $content = null ) {
 		}
 	</style>
 	
-	<div class="pipdig_left"><div id="'.$text_id.'" class="pipdig_image_side_text">'.do_shortcode($content).'</div></div>
-	
-	<div class="pipdig_right"><img id="'.$img_id.'" src="'.esc_url($image).'" alt="'.esc_attr(get_the_title()).'" /></div>
-	
-	</div>
-	
-	<div class="clearfix"></div>
-	
 	';
+	$output .= '<div class="pipdig_left">'.$left.'</div>';
+	$output .= '<div class="pipdig_right">'.$right.'</div>';
+	
+	$output .= '</div><div class="clearfix"></div>';
 	$output .= '
 	<script>
 	jQuery(window).on("load", function() {
