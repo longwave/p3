df4891ff654520c353abef97715ef6bb81318303
Phil XPS15 <philcloth@gmail.com>
2016-02-06 23:49:53 +0000

    social footer options wjlst drunk

--- inc/functions/social_footer.php
+++ inc/functions/social_footer.php
@@ -78,37 +78,37 @@ function pipdig_p3_social_footer() {
 	
 	if ($total_count) {
 	
-		if(!empty($twitter_count)) {
+		if (!empty($twitter_count) && get_theme_mod('p3_social_footer_twitter', 1)) {
 		$output .='<div '.$colz.'>';
 		$output .= '<a href="'.esc_url($links['twitter']).'" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i> Twitter | '.$twitter_count.'</a>';
 		$output .= '</div>';
 		}
 		
-		if(!empty($facebook_count)) {
+		if(!empty($facebook_count) && get_theme_mod('p3_social_footer_facebook', 1)) {
 		$output .='<div '.$colz.'>';
 		$output .= '<a href="'.esc_url($links['facebook']).'" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i> Facebook | '.$facebook_count.'</a>';
 		$output .= '</div>';
 		}
 		
-		if(!empty($instagram_count)) {
+		if(!empty($instagram_count) && get_theme_mod('p3_social_footer_instagram', 1)) {
 		$output .='<div '.$colz.'>';
 		$output .= '<a href="'.esc_url($links['instagram']).'" target="_blank" rel="nofollow"><i class="fa fa-instagram"></i> Instagram | '.$instagram_count.'</a>';
 		$output .= '</div>';
 		}
 		
-		if(!empty($youtube_count)) {
+		if(!empty($youtube_count) && get_theme_mod('p3_social_footer_youtube', 1)) {
 		$output .='<div '.$colz.'>';
 		$output .= '<a href="'.esc_url($links['youtube']).'" target="_blank" rel="nofollow"><i class="fa fa-youtube-play"></i> YouTube | '.$youtube_count.'</a>';
 		$output .= '</div>';
 		}
 		
-		if(!empty($pinterest_count)) {
+		if(!empty($pinterest_count) && get_theme_mod('p3_social_footer_pinterest', 1)) {
 		$output .='<div '.$colz.'>';
 		$output .= '<a href="'.esc_url($links['pinterest']).'" target="_blank" rel="nofollow"><i class="fa fa-pinterest"></i> Pinterest | '.$pinterest_count.'</a>';
 		$output .= '</div>';
 		}
 		
-		if(!empty($bloglovin_count)) {
+		if(!empty($bloglovin_count) && get_theme_mod('p3_social_footer_bloglovin', 1)) {
 		$output .='<div '.$colz.'>';
 		$output .= '<a href="'.esc_url($links['bloglovin']).'" target="_blank" rel="nofollow"><i class="fa fa-plus"></i> Bloglovin | '.$bloglovin_count.'</a>';
 		$output .= '</div>';
@@ -123,4 +123,144 @@ function pipdig_p3_social_footer() {
 	
 	echo $output;
 
+}
+
+
+// customiser
+if (!class_exists('pipdig_p3_social_footer_Customiser')) {
+	class pipdig_p3_social_footer_Customiser {
+		public static function register ( $wp_customize ) {
+			
+			$wp_customize->add_section( 'p3_navbar_icons_section', 
+				array(
+					'title' => __( 'Social Footer', 'p3' ),
+					'description' => __( 'Use these options to disaplay social follow icons across the footer of your site. You can also enable/disable the display of social follow counters.', 'p3' ).' <a href="//support.pipdig.co/articles/wordpress-how-to-add-social-icons-to-the-navbar/?utm_source=wordpress&utm_medium=p3&utm_campaign=customizer" target="_blank">'.__( 'Click here for more information', 'p3' ).'</a>.',
+					'capability' => 'edit_theme_options',
+					'priority' => 86,
+				) 
+			);
+
+			$wp_customize->add_setting('social_count_footer',
+				array(
+					'default' => 0,
+					'sanitize_callback' => 'absint',
+				)
+			);
+			$wp_customize->add_control(
+				'social_count_footer',
+				array(
+					'type' => 'checkbox',
+					'label' => __( 'Enable social footer', 'pipdig-arubanights' ),
+					'section' => 'pipdig_general_options',
+				)
+			);
+			
+			$wp_customize->add_setting('social_count_footer_counts',
+				array(
+					'default' => 0,
+					'sanitize_callback' => 'absint',
+				)
+			);
+			$wp_customize->add_control(
+				'social_count_footer_counts',
+				array(
+					'type' => 'checkbox',
+					'label' => __( 'Display counter stats', 'pipdig-arubanights' ),
+					'section' => 'pipdig_general_options',
+				)
+			);
+			
+			$wp_customize->add_setting('p3_social_footer_twitter',
+				array(
+					'default' => 1,
+					'sanitize_callback' => 'absint',
+				)
+			);
+			$wp_customize->add_control(
+				'p3_social_footer_twitter',
+				array(
+					'type' => 'checkbox',
+					'label' => 'Twitter',
+					'section' => 'pipdig_general_options',
+				)
+			);
+			
+			$wp_customize->add_setting('p3_social_footer_facebook',
+				array(
+					'default' => 1,
+					'sanitize_callback' => 'absint',
+				)
+			);
+			$wp_customize->add_control(
+				'p3_social_footer_facebook',
+				array(
+					'type' => 'checkbox',
+					'label' => 'Facebook',
+					'section' => 'pipdig_general_options',
+				)
+			);
+			
+			$wp_customize->add_setting('p3_social_footer_instagram',
+				array(
+					'default' => 1,
+					'sanitize_callback' => 'absint',
+				)
+			);
+			$wp_customize->add_control(
+				'p3_social_footer_instagram',
+				array(
+					'type' => 'checkbox',
+					'label' => 'Instagram',
+					'section' => 'pipdig_general_options',
+				)
+			);
+			
+			$wp_customize->add_setting('p3_social_footer_youtube',
+				array(
+					'default' => 1,
+					'sanitize_callback' => 'absint',
+				)
+			);
+			$wp_customize->add_control(
+				'p3_social_footer_youtube',
+				array(
+					'type' => 'checkbox',
+					'label' => 'YouTube',
+					'section' => 'pipdig_general_options',
+				)
+			);
+			
+			$wp_customize->add_setting('p3_social_footer_pinterest',
+				array(
+					'default' => 1,
+					'sanitize_callback' => 'absint',
+				)
+			);
+			$wp_customize->add_control(
+				'p3_social_footer_pinterest',
+				array(
+					'type' => 'checkbox',
+					'label' => 'Pinterest',
+					'section' => 'pipdig_general_options',
+				)
+			);
+			
+			$wp_customize->add_setting('p3_social_footer_bloglovin',
+				array(
+					'default' => 1,
+					'sanitize_callback' => 'absint',
+				)
+			);
+			$wp_customize->add_control(
+				'p3_social_footer_bloglovin',
+				array(
+					'type' => 'checkbox',
+					'label' => 'Bloglovin',
+					'section' => 'pipdig_general_options',
+				)
+			);
+
+		}
+	}
+	add_action( 'customize_register' , array( 'pipdig_p3_social_footer_Customiser' , 'register' ) );
 }
