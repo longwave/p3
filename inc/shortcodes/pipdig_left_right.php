c11c4ad8b245c4f3c2587201bd23969ab522b774
Philtel <philcloth@gmail.com>
2016-03-15 14:25:22 +0000

    3 col layouts

--- inc/shortcodes/pipdig_left_right.php
+++ inc/shortcodes/pipdig_left_right.php
@@ -15,3 +15,24 @@ function pipdig_p3_shortcode_right( $atts, $content = null ) {
 	return '<div class="pipdig_right">'.do_shortcode($content).'</div><div class="clearfix"></div>';
 }
 add_shortcode( 'pipdig_right', 'pipdig_p3_shortcode_right' );
+
+
+
+
+// [pipdig_col1] [/pipdig_col1]
+function pipdig_p3_shortcode_col1( $atts, $content = null ) {
+	return '<div class="pipdig_col1">'.do_shortcode($content).'</div>';
+}
+add_shortcode( 'pipdig_col1', 'pipdig_p3_shortcode_col1' );
+
+// [pipdig_col2] [/pipdig_col2]
+function pipdig_p3_shortcode_col2( $atts, $content = null ) {
+	return '<div class="pipdig_col2">'.do_shortcode($content).'</div>';
+}
+add_shortcode( 'pipdig_col2', 'pipdig_p3_shortcode_col2' );
+
+// [pipdig_col3] [/pipdig_col3]
+function pipdig_p3_shortcode_col3( $atts, $content = null ) {
+	return '<div class="pipdig_col3">'.do_shortcode($content).'</div><div class="clearfix"></div>';
+}
+add_shortcode( 'pipdig_col3', 'pipdig_p3_shortcode_col3' );
