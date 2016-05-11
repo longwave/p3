510c0a89b4bd3caf6e2564b58ae8dd634b736bbb
Philtel <philcloth@gmail.com>
2016-05-11 17:57:02 +0100

    UNBREAK ON PHP <5.5 waah

--- p3.php
+++ p3.php
@@ -5,7 +5,7 @@ Plugin URI: https://www.pipdig.co/
 Description: The core functions of any pipdig theme.
 Author: pipdig
 Author URI: https://www.pipdig.co/
-Version: 2.1.9
+Version: 2.1.10
 Text Domain: p3
 */
 
@@ -13,7 +13,7 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-define( 'PIPDIG_P3_V', '2.1.9' );
+define( 'PIPDIG_P3_V', '2.1.10' );
 
 $clasped = '';
 function p3_falcor() {
