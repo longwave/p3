14fd226ef9b86c7044314668a97fcb0c2f3086df
Philtel <philcloth@gmail.com>
2016-06-06 13:37:54 +0100

    pipdig_banner shortcode

---
+++ inc/shortcodes/pipdig_banner.php
@@ -0,0 +1,28 @@
+<?php 
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+// [pipdig_banner image=""]
+function parallax_section_func( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+		'image' => '',
+		'parallax' => '',
+	), $atts ) );
+	
+	if (empty($image)) {
+		return;
+	}
+	
+	$output = '';
+	
+	if ($parallax == 'yes') {
+		$parallax_class = ' pipdig_banner_parallax';
+	}
+	
+	$output .= '<div class="pipdig_banner'.$parallax_class.'" style="background-image:url('.esc_url($image).');" data-stellar-background-ratio="1.4"></div>';
+	
+	return $output;
+}
+add_shortcode( 'pipdig_banner', 'parallax_section_func' );
