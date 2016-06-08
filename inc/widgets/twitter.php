baa01f5d48ffd7e6fef1127f987e1274bf63e788
Philtel <philcloth@gmail.com>
2016-06-08 18:39:42 +0100

    2.3.8 - new twitter widget which doesn't require ID. woop.

--- inc/widgets/twitter.php
+++ inc/widgets/twitter.php
@@ -16,15 +16,16 @@ if ( !class_exists( 'pipdig_widget_twitter' ) ) {
 		// PART 1: Extracting the arguments + getting the values
 		extract($args, EXTR_SKIP);
 		$title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']);
+		
+		$twitter_handle = '';
 		if (isset($instance['twitter_handle'])) { 
 			$twitter_handle = esc_attr($instance['twitter_handle']);
-		} else {
-			$twitter_handle = '';
 		}
-		if (isset($instance['twitter_widget_id'])) { 
-			$twitter_widget_id = $instance['twitter_widget_id'];
+		
+		if (isset($instance['number'])) { 
+			$number = absint($instance['number']);
 		} else {
-			$twitter_widget_id = '';
+			$number = 3;
 		}
 
 		// Before widget code, if any
@@ -34,12 +35,10 @@ if ( !class_exists( 'pipdig_widget_twitter' ) ) {
 		if (!empty($title)) {
 			echo $before_title . $title . $after_title;
 		}
-		
-		
 
-		if (!empty($twitter_handle) && !empty($twitter_widget_id)) { ?>
+		if (!empty($twitter_handle)) { ?>
 			<?php $twitter_handle = str_replace('@', '', $twitter_handle); ?>
-			<a class="twitter-timeline" data-tweet-limit="2" data-dnt="true" data-border-color="<?php echo get_theme_mod( 'content_background_color', '#ffffff'); ?>" data-chrome="noheader nofooter" href="https://twitter.com/<?php echo $twitter_handle; ?>" data-widget-id="<?php echo $twitter_widget_id; ?>">Tweets by @<?php echo $twitter_handle; ?></a>
+			<a class="twitter-timeline" href="https://twitter.com/<?php echo $twitter_handle; ?>" data-tweet-limit="<?php echo $number; ?>">Tweets by <?php echo $twitter_handle; ?></a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 			<br />
 			<a href="https://twitter.com/<?php echo $twitter_handle; ?>" class="twitter-follow-button" data-show-count="true" data-dnt="true">Follow @<?php echo $twitter_handle; ?></a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>		
@@ -68,11 +67,12 @@ if ( !class_exists( 'pipdig_widget_twitter' ) ) {
 			}
 		}
 		
-		if (isset($instance['twitter_widget_id'])) { 
-			$twitter_widget_id = $instance['twitter_widget_id'];
+		if (isset($instance['number'])) { 
+			$number = absint($instance['number']);
 		} else {
-			$twitter_widget_id = '';
+			$number = 3;
 		}
+
 		// PART 2-3: Display the fields
 		?>
 		
@@ -84,11 +84,11 @@ if ( !class_exists( 'pipdig_widget_twitter' ) ) {
 			<label for="<?php echo $this->get_field_id('twitter_handle'); ?>"><?php _e('Twitter Username (Not including @ symbol):', 'p3'); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id('twitter_handle'); ?>" name="<?php echo $this->get_field_name('twitter_handle'); ?>" type="text" value="<?php echo esc_attr($twitter_handle); ?>" placeholder="e.g. pipdig" />
 		</p>
-		<p><a href="//support.pipdig.co/articles/wordpress-twitter-widget/?utm_source=wordpress&utm_medium=p3&utm_campaign=widget" target="_blank"><?php _e('Click here for information', 'p3'); ?></a></p>
 		<p>
-			<label for="<?php echo $this->get_field_id('twitter_widget_id'); ?>"><?php _e('Twitter Widget ID:', 'p3'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('twitter_widget_id'); ?>" name="<?php echo $this->get_field_name('twitter_widget_id'); ?>" type="text" value="<?php echo esc_attr($twitter_widget_id); ?>" placeholder="e.g. 123456789" />
+			<label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of Tweets', 'p3'); ?></label>
+			<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="number" min="1" max="10" value="<?php echo absint($number); ?>" />
 		</p>
+		<p><a href="//support.pipdig.co/articles/wordpress-twitter-widget/?utm_source=wordpress&utm_medium=p3&utm_campaign=widget" target="_blank"><?php _e('Click here for information', 'p3'); ?></a></p>
 		
 		<?php
 	   
@@ -98,7 +98,7 @@ if ( !class_exists( 'pipdig_widget_twitter' ) ) {
 		$instance = $old_instance;
 		$instance['title'] = esc_attr($new_instance['title']);
 		$instance['twitter_handle'] = esc_attr($new_instance['twitter_handle']);
-		$instance['twitter_widget_id'] = esc_attr($new_instance['twitter_widget_id']);
+		$instance['number'] = absint($new_instance['number']);
 		return $instance;
 	  }
 
