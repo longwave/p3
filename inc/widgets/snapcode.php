d9d422a9de4127ff21bfc7801e27577bb027110e
Philtel <philcloth@gmail.com>
2016-12-19 16:34:22 +0000

    change to new image widget that can use customizer

--- inc/widgets/snapcode.php
+++ inc/widgets/snapcode.php
@@ -1,92 +1,126 @@
 <?php
-
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+/**
+ * Image Upload Widget
+ */
 class pipdig_p3_snapchat_snapcode extends WP_Widget {
- 
-  public function __construct() {
-      $widget_ops = array('classname' => 'pipdig_p3_snapchat_snapcode', 'description' => __("Display your Snapchat Snapcode (QR code).", 'p3') );
-      parent::__construct('pipdig_p3_snapchat_snapcode', 'pipdig - ' . 'Snapchat Snapcode', $widget_ops);
-  }
-  
-  function widget($args, $instance) {
-    // PART 1: Extracting the arguments + getting the values
-    extract($args, EXTR_SKIP);
-    $title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']);
-    $snapchat_account = empty($instance['snapchat_account']) ? '' : trim($instance['snapchat_account']);
-	$snapcode = empty($instance['snapcode']) ? '' : $instance['snapcode'];
-	$snap_url = esc_url('https://www.snapchat.com/add/'.$snapchat_account);
-	
-    // Before widget code, if any
-    echo (isset($before_widget)?$before_widget:'');
-   
-    // PART 2: The title and the text output
-    if (!empty($title)) {
-		echo $before_title . esc_html($title) . $after_title;
-	}
-	
-    if (!empty($snapcode)) { ?>
-		
-		<?php if (!empty($snapchat_account)) { ?><a href="<?php echo $snap_url; ?>" target="_blank" rel="nofollow"><?php } ?>
-			<img src="<?php echo esc_url($snapcode); ?>" style="min-width: 1in; max-width: 1.2in; height: auto;" alt="<?php echo esc_attr($snapchat_account); ?> on Snapchat" />
-		<?php if (!empty($snapchat_account)) { ?></a>
-			<p><?php printf( __('Follow <b>%s</b> on Snapchat!', 'p3'), strip_tags($snapchat_account) ); ?></p>
-		<?php } ?>
-	<?php } else {
-		_e("Setup not complete. Please check the widget options.", 'p3');
-	}
-    // After widget code, if any
-    echo (isset($after_widget)?$after_widget:'');
-  }
- 
-  public function form( $instance ) {
-   
-     // PART 1: Extract the data from the instance variable
-     $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
-     $title = $instance['title'];
-	 
-	 if (isset($instance['snapchat_account'])) {
-		$snapchat_account = $instance['snapchat_account'];
-	 } else {
-		 $snapchat_account = '';
-	 }
 
-     // PART 2-3: Display the fields
-     ?>
-	<p>
-		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'p3'); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-	</p>
-	
-		<p style="font-weight:bold"><?php _e('Upload your <a href="https://accounts.snapchat.com/accounts/snapcodes?type=png" target="_blank">Snapcode</a> PNG file:', 'p3'); ?></p>
-		<?php
-			if (!empty($instance['snapcode'])) {
-				echo '<img src="' . $instance['snapcode'] . '" style="margin:5px 0;padding:0;max-width:150px;height:auto" alt="" /><br />';
+	// Holds widget settings defaults, populated in constructor.
+	protected $defaults;
+
+	// Constructor. Set the default widget options and create widget.
+	function __construct() {
+
+		$this->defaults = array(
+			'title' => '',
+			'snapcode' => '',
+			'link' => '',
+			'target' => '',
+		);
+
+		$widget_ops = array(
+			'classname' => 'pipdig_p3_snapchat_snapcode',
+			'description' => __('Display your Snapchat Snapcode.', 'wpshed'),
+		);
+
+		$control_ops = array(
+			'id_base' => 'pipdig_p3_snapchat_snapcode',
+			'width'   => 200,
+			'height'  => 250,
+		);
+
+		parent::__construct('pipdig_p3_snapchat_snapcode', 'pipdig - Snapchat Snapcode', $widget_ops, $control_ops);
+
+	}
+
+	// The widget content.
+	function widget($args, $instance) {
+
+		//* Merge with defaults
+		$instance = wp_parse_args((array) $instance, $this->defaults);
+
+		echo $args['before_widget'];
+
+			if (! empty($instance['title']))
+				echo $args['before_title'] . apply_filters('widget_title', $instance['title'], $instance, $this->id_base) . $args['after_title'];
+			
+			$link_open = $link_close = '';
+			if (!empty($instance['snapchat_account'])) {
+				$link_open = '<a href="'.esc_url('https://www.snapchat.com/add/'.trim($instance['snapchat_account'])).'" target="_blank" rel="nofollow">';
+				$link_close = '</a>';
 			}
+
+			if (!empty($instance['snapcode'])) {
+				echo $link_open.'<img src="'.esc_url($instance['snapcode']).'" alt="Snapchat" style="min-width: 1.1in; max-width: 1.3in; height: auto;"  />'.$link_close;
+				if (!empty($instance['snapchat_account'])) {
+					echo '<p>'.sprintf( __('Follow <b>%s</b> on Snapchat!', 'p3'), strip_tags($instance['snapchat_account']) ).'</p>';
+				}
+			}
+			
+		echo $args['after_widget'];
+
+	}
+
+	// Update a particular instance.
+	function update($new_instance, $old_instance) {
+
+		$new_instance['title'] = strip_tags($new_instance['title']);
+		$new_instance['snapcode'] = strip_tags($new_instance['snapcode']);
+		$new_instance['snapchat_account'] = strip_tags($new_instance['snapchat_account']);
+
+		return $new_instance;
+
+	}
+
+	// The settings update form.
+	function form($instance) {
+
+		// Merge with defaults
+		$instance = wp_parse_args((array) $instance, $this->defaults);
+
 		?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+			<input type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo esc_attr($instance['title']); ?>" class="widefat" />
+		</p>
+		
+		<p>1. Download your Snapcode PNG image from <a href="https://accounts.snapchat.com/accounts/snapcodes?type=png" target="_blank">this link</a></p>
+		<p>2. Upload your Snapcode PNG image using the button below.</p>
 
-		<input type="text" style="display:none" class="widefat custom_media_url" name="<?php echo $this->get_field_name('snapcode'); ?>" id="<?php echo $this->get_field_id('snapcode'); ?>" value="<?php if (isset($instance['snapcode'])) { echo $instance['snapcode']; } ?>" style="margin-top:5px;">
+		<p>
+			<div class="pipdig-media-container">
+				<div class="pipdig-media-inner">
+					<?php $img_style = ($instance[ 'snapcode' ] != '') ? '' : 'display:none;'; ?>
+					<img id="<?php echo $this->get_field_id('snapcode'); ?>-preview" src="<?php echo esc_attr($instance['snapcode']); ?>" style="margin:5px 0;padding:0;max-width:150px;height:auto;<?php echo $img_style; ?>" />
+					<?php $no_img_style = ($instance[ 'snapcode' ] != '') ? 'style="display:none;"' : ''; ?>
+				</div>
+			
+				<input type="text" id="<?php echo $this->get_field_id('snapcode'); ?>" name="<?php echo $this->get_field_name('snapcode'); ?>" value="<?php echo esc_attr($instance['snapcode']); ?>" class="pipdig-media-url" style="display: none" />
 
-		<input type="button" class="button button-primary custom_media_button" id="custom_media_button" name="<?php echo $this->get_field_name('snapcode'); ?>" value="<?php _e('Upload Snapcode', 'p3'); ?>" style="margin-top:8px;" />
+				<input type="button" value="<?php echo esc_attr(__('Remove', 'p3')); ?>" class="button pipdig-media-remove" id="<?php echo $this->get_field_id('snapcode'); ?>-remove" style="<?php echo $img_style; ?>" />
 
-	<p>
-		<label for="<?php echo $this->get_field_id('snapchat_account'); ?>"><?php _e("Snapchat Account Name:", 'p3'); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('snapchat_account'); ?>" name="<?php echo $this->get_field_name('snapchat_account'); ?>" type="text" value="<?php echo esc_attr($snapchat_account); ?>" placeholder="<?php _e("For example:", 'p3'); ?> mileyxxcyrus" />
-	</p>
-	
-	
-     <?php
-   
-  }
- 
-  function update($new_instance, $old_instance) {
-    $instance = $old_instance;
-	$instance['title'] = strip_tags($new_instance['title']);
-	$instance['snapcode'] = strip_tags($new_instance['snapcode']);
-    $instance['snapchat_account'] = strip_tags($new_instance['snapchat_account']);
-	update_option('pipdig_p3_snapchat_account', $instance['snapchat_account']); // might use this in a shortcode later
-    return $instance;
-  }
-  
+				<input type="button" value="<?php echo esc_attr(__('Select Image', 'p3')); ?>" class="button pipdig-media-upload" id="<?php echo $this->get_field_id('snapcode'); ?>-button" />
+				<br class="clear">
+			</div>
+		</p>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id('snapchat_account'); ?>"><?php _e('Snapchat Account Name:', 'wpshed'); ?></label>
+			<input type="text" id="<?php echo $this->get_field_id('snapchat_account'); ?>" name="<?php echo $this->get_field_name('snapchat_account'); ?>" value="<?php if (isset($instance['snapchat_account'])) echo esc_attr($instance['snapchat_account']); ?>" class="widefat" placeholder="<?php _e("For example:", 'p3'); ?> mileyxxcyrus" />
+		</p>
+
+		<?php
+
+	}
+
 }
-add_action( 'widgets_init', create_function('', 'return register_widget("pipdig_p3_snapchat_snapcode");') );
+
+
+/**
+ * Register Widget
+ */
+function register_pipdig_p3_snapchat_widget() { 
+	register_widget('pipdig_p3_snapchat_snapcode');
+}
+add_action('widgets_init', 'register_pipdig_p3_snapchat_widget');
