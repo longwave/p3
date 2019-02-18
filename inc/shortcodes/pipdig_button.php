e44334077eaa1fd0d5c468321bf8e1ed67ba42c7
Phil <philcloth@gmail.com>
2019-02-18 13:13:51 +0000

    1

--- inc/shortcodes/pipdig_button.php
+++ inc/shortcodes/pipdig_button.php
@@ -6,12 +6,14 @@ function pipdig_button_shortcode_function( $atts, $content = null ) {
 	extract( shortcode_atts( array(
 		'title' => '',
 		'link' => '',
+		'target' => '_self',
+		'rel' => 'noopener'
 	), $atts ) );
 	
 	$output = '';
 	
 	if ($link && $title) {
-		$output = '<div class="pipdig_button_shortcode"><a class="more-link" href="'.esc_url($link).'">'.esc_html($title).'</a></div>';
+		$output = '<div class="pipdig_button_shortcode"><a class="more-link" href="'.esc_url($link).'" target="'.esc_attr($target).'" rel="'.esc_attr($rel).'">'.esc_html($title).'</a></div>';
 	}
 	
 	return $output;
